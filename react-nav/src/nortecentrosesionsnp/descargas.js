617e9f650d42c1bd24b997f4f3df4cf357d5ce18
YahairaPerez1994 <yahairaperez2694@gmail.com>
2024-03-29 00:17:55 -0500

    Popup de descargas listo

--- react-nav/src/nortecentrosesionsnp/descargas.js
+++ react-nav/src/nortecentrosesionsnp/descargas.js
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from 'react';
+import './ubicacioncarpeta.css'; // Asegúrate de importar los estilos CSS adecuados
+import PopupMessage from './popupmessage'; // Importa el componente PopupMessage
 
 function Descargas() {
   const [temporadas, setTemporadas] = useState([]);
   const [selectedTemporada, setSelectedTemporada] = useState('');
   const [rankingData, setRankingData] = useState(null);
   const [error, setError] = useState('');
+  const [confirmationMessage, setConfirmationMessage] = useState('');
+  const [showPopup, setShowPopup] = useState(false); // Estado para controlar la visibilidad del mensaje emergente
   const apiKey = 'GafXD93ZXV3jbslFcBaXT1ALLcKkBBG04JP9ZmCO';
 
   useEffect(() => {
@@ -57,11 +61,18 @@ function Descargas() {
       
       // Abrir automáticamente el enlace de descarga en una nueva ventana
       if (data.LinkDeDescarga) {
+        setConfirmationMessage('La solicitud de descarga se realizó exitosamente.');
+        setShowPopup(true); // Mostrar el mensaje emergente
         window.open(data.LinkDeDescarga, '_blank');
       }
     } catch (error) {
       console.error('Error fetching ranking data:', error);
     }
+  };
+
+  const closePopup = () => {
+    setShowPopup(false); // Ocultar el mensaje emergente al cerrarlo
+    setConfirmationMessage('');
   };
 
   return (
@@ -75,8 +86,9 @@ function Descargas() {
           </option>
         ))}
       </select>
-      <button onClick={handleDownload}>Descarga Ranking</button>
+      <button onClick={handleDownload}>Descargar</button>
       {error && <p style={{ color: 'red' }}>{error}</p>}
+      {showPopup && <PopupMessage message={confirmationMessage} onClose={closePopup} />} {/* Mostrar el mensaje emergente */}
       {rankingData && (
         <div>
           {/* Renderizar los datos de ranking aquí */}
